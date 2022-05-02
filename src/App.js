badd2ed6961645be41d95c7a6480b0e1050e5b24
IamDexterous <harshilprajapatiofficial@gmail.com>
2022-05-02 19:09:12 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
